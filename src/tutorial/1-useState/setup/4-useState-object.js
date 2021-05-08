21d88c65358cf7fc03b0d536da6288dd4adeea2c
dhruvinfo28 <dhruv.info28@gmail.com>
2021-05-09 02:50:42 +0530

    Object and Counter

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
+
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  let [person,setPerson] = useState({name:'Dhruvee', age:21, title: 'ABC'})
+  const changeTitle = ()=>{
+    //Passing the same person using the spread operator(techincally spreading its properties and then)
+    //Overwriting the title property
+    setPerson({...person,title: 'New title'})
+
+    //Note: You can have any number of state variables 
+  }
+  return (
+    <>
+    <h2>{person.name}</h2>
+    <h2>{person.age}</h2>
+    <h3>{person.title}</h3>
+    <button className="btn" onClick={changeTitle}> Change title</button>
+    </>
+  );
 };
 
 export default UseStateObject;
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,16 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  let [value,setValue] = useState(0);
+  return <>
+  <section style={{margin:"4rem"}}>
+    <h1>
+      Counter
+    </h1>
+    <h2>{value}</h2>
+    <button className="btn" onClick={()=>{setValue(value+1)}}>Increase</button>
+  </section>
+  </>;
 };
 
 export default UseStateCounter;
