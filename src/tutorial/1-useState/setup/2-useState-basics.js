9022b70829ff7a395ef14c8917298de6c016b2cf
dhruvinfo28 <dhruv.info28@gmail.com>
2021-05-08 21:49:55 +0530

    useState var and array

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,47 @@
 import React, { useState } from 'react';
 
+//Here you can see useState is a named export
+//useState is a function , that helps us change the state of any javascript variable and the rerender the component
+//On invoking sets the default value for a variable
+
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  //It returns a default value for the variable and a function to control that variable as an array 
+  //of two elements one the var(Js object) and other the function
+  //The parameter is the default value of title variable
+  //Note that setTitle is just a naming convention , you can name it anything 
+
+  let [title,setTitle] = useState('Random Title');
+  const clickHandler = ()=>{
+    //Notice doing this won't change the title, as we donot render the component 
+    //And then on restarting we will still see Random Title as we were not able to preserve the
+    //State of title variable
+    // title = 'Hello World';
+
+    //This would do it
+    // setTitle('Hello world');
+
+    //Toggling 
+    if(title==='Random Title'){
+      setTitle(69)
+    }else{
+      setTitle('Random Title');
+    }
+  }
+  return (
+    <>
+      <h1>{title}</h1>
+      <button className='btn' onClick={clickHandler}> Change title</button>
+    </>
+  );
 };
 
 export default UseStateBasics;
+
+//Few points to remember:
+//Hooks can only be invoked inside components
+
+//Components using hooks have to be upper case, we can have lower cased components but we need to import them with a name that has first letter Capital
+
+//Hooks always start with a use
+
+//Cannot call a hook conditionally(Note not talking about the function that the hook returns )
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+
+  let [arr,setArr] = useState(data);
+  
+  const removeElement = (id)=>{
+    //Filter returns a new arr with items not having id equal to the one to be deleted
+      setArr(arr.filter(item=> item.id != id));
+  }
+  return (
+    <>
+    {arr.map(item=>{
+      return <div key={item.id} className="item">
+          <h4>{item.name}</h4>
+          <button onClick={()=>{removeElement(item.id)}}>Remove</button>
+      </div>
+    })}
+    <button className='btn' onClick={()=>{setArr([])}}>Clear array</button>
+    </>
+  );
 };
 
 export default UseStateArray;
