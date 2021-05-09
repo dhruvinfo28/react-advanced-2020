358c5f460b487f1f62418e3733dc242539092a9b
dhruvinfo28 <dhruv.info28@gmail.com>
2021-05-09 14:40:11 +0530

    Adding using useState and using callback in setState

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -7,17 +7,32 @@ const UseStateArray = () => {
   
   const removeElement = (id)=>{
     //Filter returns a new arr with items not having id equal to the one to be deleted
-      setArr(arr.filter(item=> item.id != id));
+      // setArr(arr.filter(item=> item.id != id));
+      //The function method of doing
+      //Whatever the callback inside the setArr returns will be the new value
+      //SInce curly braces are not used in the callback meaning it returns wahtever filter returns
+      //Your app will crash if the callback doesn't return a value
+      setArr((prevArr)=>prevArr.filter(item=> item.id != id));
   }
+  
+  //Whenver you wish to add an item never do prev.something = some or use push 
+  //Always treat the state variable as immutable and use the setState() method only with spread
+  //Operator to pass in the new Value of the variable
+  // const addItem = ()=>{
+  //   setArr((prevArr)=>{
+  //       return [...prevArr, {id:6, name: "Dhruvee"}];
+  //   })
+  // }
   return (
     <>
     {arr.map(item=>{
-      return <div key={item.id} className="item">
+      return <div className="item">
           <h4>{item.name}</h4>
           <button onClick={()=>{removeElement(item.id)}}>Remove</button>
       </div>
     })}
     <button className='btn' onClick={()=>{setArr([])}}>Clear array</button>
+    {/* <button className='btn' onClick={addItem}>Add Item</button> */}
     </>
   );
 };
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -2,6 +2,19 @@ import React, { useState } from 'react';
 
 const UseStateCounter = () => {
   let [value,setValue] = useState(0);
+  const complexIncrease = ()=>{
+    setTimeout(()=>{
+      //This setValue is an asynchronous function, hence it sees the value as whatever the previous 
+      //Value was , so if we click this button like 10 times in 1 sec the counter would only increase
+      //by one as in all the calls value was seen as one
+        // setValue(value+1);
+
+        //To overcome
+        setValue((prevState)=>{
+          return prevState+1;
+        })
+    },1000)
+  }
   return <>
   <section style={{margin:"4rem"}}>
     <h1>
@@ -9,6 +22,11 @@ const UseStateCounter = () => {
     </h1>
     <h2>{value}</h2>
     <button className="btn" onClick={()=>{setValue(value+1)}}>Increase</button>
+  </section>
+  <section styel={{margin:"4rem"}}>
+    <h1>Complex counter</h1>
+    <h2>{value}</h2>
+    <button className="btn" onClick={complexIncrease}>Complex Counter</button>
   </section>
   </>;
 };
