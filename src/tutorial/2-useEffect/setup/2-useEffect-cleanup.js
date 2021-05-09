6289f7b2cd0599c8d255912305e6f5705b50abbb
dhruvinfo28 <dhruv.info28@gmail.com>
2021-05-10 01:07:47 +0530

    useEffect done

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,34 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size,setSize] = useState(window.innerWidth);
+  
+  const updateSize = ()=>{
+    setSize(window.innerWidth);
+  }
+
+  // useEffect(()=>{
+  //   window.addEventListener('resize',updateSize);
+  // },[])
+  //We only want to add the event listener once . If we dont do that on every window change setSize 
+  //called component rerenders and we add a new event listener 
+  
+  //Or we can return a cleanup function 
+  useEffect(()=>{
+    console.log('use Effect')
+    window.addEventListener('resize',updateSize);
+    return ()=>{
+      //This function will run before useEffect(if useEffect has run once)
+      console.log('cleanup')
+      window.removeEventListener('resize',updateSize);
+    }
+  })
+  //render coming two times because of strict mode
+  console.log('render')
+  return <>
+  <h1>Window Size</h1>
+  <h2>{size}PX</h2>
+  </>;
 };
 
 export default UseEffectCleanup;
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,7 +3,41 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users,setUsers] = useState([]);
+
+  const getData = async()=>{
+    const response = await fetch(url)
+    const data = await response.json()
+    //setUsers(data)
+    //The above thing will result in an infinite loop as setUsers causes a rerender and after 
+    //every renrender runs a useEffect which again calls getData which again triggers a rerender
+    //by calling setUsers
+
+    //So we use the second parameter to only run on initial rerender
+    setUsers(data)
+    // console.log(data);
+  }
+  useEffect(()=>{
+      getData();
+  },[])
+  return <>
+    <h3>Github users</h3>
+    <ul className="users">
+      {
+        users.map(user=>{
+          const {id,avatar_url,login} = user;
+            return <li key={id}>
+                <img src={avatar_url} alt="" />
+                <div>
+                  <h4>
+                    {login}
+                  </h4>
+                </div>
+            </li>;
+        })
+      }
+    </ul>
+  </>;
 };
 
 export default UseEffectFetchData;
