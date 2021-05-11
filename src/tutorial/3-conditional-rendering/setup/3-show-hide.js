fcff91dfaa5c7c7fb3449b7b471cee592b97e1dc
dhruvinfo28 <dhruv.info28@gmail.com>
2021-05-11 18:52:43 +0530

    True need of cleanup, conditional rendering

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show,setShow] = useState(false);
+  return <>
+  <button className="btn" onClick={()=>{setShow(!show)}}>Show/hide</button>
+  {show && <Item />}
+  </>
 };
+
+const Item = ()=>{
+  const [size,setSize] = useState(window.innerWidth);
+  const checkSize = ()=>{
+    setSize(window.innerWidth);
+  }
+  useEffect(()=>{
+    window.addEventListener('resize', checkSize);
+
+    return ()=>{
+      window.removeEventListener('resize',checkSize);
+    }
+  },[])
+  //Here initial render wont save us as the component is toggled
+  //(<Item/> comes in the picture and goes out) hence getting back <Item/> is an inital render
+  //Because we are not causing a rerender of item from a state variable
+  // so when it is toggled back on
+  //it is similar to an initial render and hence we will have multiple event listeners , hence 
+  //we need a cleanup function
+  return<>
+    <h1>Window size:</h1>
+    <h3>Size: {size} px</h3>
+  </>;
+}
 
 export default ShowHide;
