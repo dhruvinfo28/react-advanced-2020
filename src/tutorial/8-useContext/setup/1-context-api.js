c59791214669d466b3f1845b6bc48bd6c24a0763
dhruvinfo28 <dhruv.info28@gmail.com>
2021-05-14 18:34:47 +0530

    Context Api

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -3,6 +3,14 @@ import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+
+//Using context api we dont have to pass the props down the component tree they are made available
+
+const PersonContext = React.createContext();
+//Now you want to wrap your root component with this
+//Returns a provider and a consumer
+//Wrap root with the provider
+
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
   const removePerson = (id) => {
@@ -11,14 +19,25 @@ const ContextAPI = () => {
     });
   };
   return (
-    <>
+    <PersonContext.Provider value={{removePerson, people}}>
+      {/**<PersonContext.Provider value={removePerson} > 
+       * Above shows passing down one prop
+       * to pass many use PersonContext.Provider value={{prop1,prop2,prop3}}
+       * And then destructure it as
+       * const {prop1,prop2,prop3} = useContext(PersonContext);
+       */}
       <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
+      {/** now that we have context we dont need to pass down the props every level, just give them in the root
+       * Context comes in handy when there is a depth of certain level
+       */}
+      {/* <List people={people} removePerson={removePerson} /> */}
+      <List  />
+    </PersonContext.Provider>
   );
 };
 
-const List = ({ people, removePerson }) => {
+const List = () => {
+  const {people} = useContext(PersonContext);
   return (
     <>
       {people.map((person) => {
@@ -26,7 +45,7 @@ const List = ({ people, removePerson }) => {
           <SinglePerson
             key={person.id}
             {...person}
-            removePerson={removePerson}
+            // removePerson={removePerson}
           />
         );
       })}
@@ -34,7 +53,9 @@ const List = ({ people, removePerson }) => {
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  //And use the prop like this
+  const {removePerson} = useContext(PersonContext);
   return (
     <div className='item'>
       <h4>{name}</h4>
