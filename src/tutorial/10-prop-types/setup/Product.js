21e6a022f4a98d96387df11321b1177fee9e8423
dhruvinfo28 <dhruv.info28@gmail.com>
2021-05-16 00:26:40 +0530

    prop types and router basics

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,33 @@
 import React from 'react';
-
-const Product = () => {
-  return <article className='product'>single product</article>;
+import PropTypes from 'prop-types'
+import defaultImage from '../../../assets/default-image.jpeg'
+const Product = ({image,name,price}) => {
+  const url = image && image.url;
+  return <article className='product'>
+    <img src={url || defaultImage} alt="" />
+    <h4>{name || 'default name'}</h4>
+    <p>{price || 3.99}</p>
+    </article>;
 };
 
+//After setting the prop types and making them required we are going to have react scream at us 
+//saying it was requried but is undefined 
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired
+}
+
+//So to tackle we can have default props 
+
+// Product.defaultProps = {
+//   name:'Default name',
+//   price: 3.99,
+//   image: {
+//     url: defaultImage
+//   }
+// }
+
+//So now for the last product the image and the price was missing and now it renders the default values
 export default Product;
--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -10,7 +10,20 @@ import Person from './Person';
 // navbar
 import Navbar from './Navbar';
 const ReactRouterSetup = () => {
-  return <h2>react router</h2>;
+  //Wrap your entire application inside the BrowserRouter from react-router-dom package
+  return <Router>
+    {/** If we dont add the exact prop then / will be displayed in /about as well as /people as the '/' matches */}
+    <Route exact path="/">
+      <Home />
+    </Route>
+    {/* If we dont add exact here all the pages with /about/anything/dfvd/f will render this component */}
+    <Route exact path="/about">
+      <About />
+    </Route>
+    <Route exact path="/people">
+      <People />
+    </Route>
+  </Router>;
 };
 
 export default ReactRouterSetup;
